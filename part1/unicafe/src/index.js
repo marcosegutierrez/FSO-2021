2499a69a771ae7dd4aaaa1cae292ae85b240f680
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2022-02-22 22:48:04 -0300

    Ejercicio 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -15,17 +15,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [avgScore, setAvgScore] = useState(0)
 
   const setToGood = (newValue) => {
     setGood(newValue)
+    setAll(all+1)
+    setAvgScore(avgScore+1)
+    setToAvg(avgScore)
   }
 
   const setToNeutral = (newValue) => {
     setNeutral(newValue)
+    setAll(all+1)
+    setAvgScore(avgScore)
+    setToAvg(avgScore)
   }
 
   const setToBad = (newValue) => {
     setBad(newValue)
+    setAll(all+1)
+    setAvgScore(avgScore-1)
+    setToAvg(avgScore)
+  }
+
+  const setToAvg = (avgValue) => {
+    if (all !== 0){
+      setAvg(avgValue/all)
+    }
+    else setAvg(avgValue)
   }
 
   return (
@@ -38,6 +57,8 @@ const App = () => {
       <Display text = 'Good' state = {good} />
       <Display text = 'Neutral' state = {neutral} />
       <Display text = 'Bad' state = {bad} />
+      <Display text = 'All' state = {all} />
+      <Display text = 'Average' state = {avg} />
     </div>
   )
 }
